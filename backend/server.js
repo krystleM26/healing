fe936da816f518d0b174e6e54e7c7f436474e58b
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-08-24 16:29:28 -0700

    Connected to db

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import data from './data.js'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log('connected to db')
+})
+
+const app = express()
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products)
+})
+app.get('/api/products/slug/:slug', (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug)
+  if (product) {
+    res.send(product)
+  } else {
+    res.status(404).send({ message: 'Product Not Found' })
+  }
+  res.send(data.products)
+})
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`)
+})
